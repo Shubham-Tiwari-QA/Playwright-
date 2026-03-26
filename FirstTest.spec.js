041319a5f2c452f92ea4350111b7876bf88f3a40
SHUBHAM TIWARI QA ENGINEER <shubhamtiwari3457@gmail.com>
2026-03-26 10:13:41 +0530

    Update FirstTest.spec.js

--- FirstTest.spec.js
+++ FirstTest.spec.js
@@ -1,19 +1,11 @@
-// @ts-check
 import { test, expect } from '@playwright/test';
 
-test('open google page and check title', async ({ page }) => {
+test('open google page and check title @smoke', async ({ page }) => {
 
-  // Open website
   await page.goto("https://www.google.com");
 
-  // Get Page Title
-  const pageTitle = await page.title();
-  console.log("Page Title is:", pageTitle);
-
-  // Get Page URL
-  const pageUrl = page.url();
-  console.log("Page URL is:", pageUrl);
-
-  // Assertion
   await expect(page).toHaveTitle(/Google/);
+
+  console.log("Page Title is:", await page.title());
+  console.log("Page URL is:", page.url());
 });
